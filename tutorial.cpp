9fa8ef7fff12ac9ccf8632dcd78fbb7bee245b4d
trallen22 <77846813+trallen22@users.noreply.github.com>
2025-02-27 23:35:27 -0500

    changed some formatting

--- tutorial.cpp
+++ tutorial.cpp
@@ -49,22 +49,16 @@ bool init()
 	bool success = true;
 
 	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
+	if ( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
 		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
 		success = false;
-	}
-	else
-	{
+	} else {
 		//Create window
 		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
+		if ( gWindow == NULL ) {
 			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
 			success = false;
-		}
-		else
-		{
+		} else {
 			//Get window surface
 			gScreenSurface = SDL_GetWindowSurface( gWindow );
 		}
@@ -80,40 +74,35 @@ bool loadMedia()
 
 	//Load default surface
 	gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ] = loadSurface( "images/cartoon_toast.png" );
-	if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ] == NULL )
-	{
+	if ( gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ] == NULL ) {
 		printf( "Failed to load default image!\n" );
 		success = false;
 	}
 
 	//Load up surface
 	gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ] = loadSurface( "images/cartoon_toast.png" );
-	if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ] == NULL )
-	{
+	if ( gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ] == NULL ) {
 		printf( "Failed to load up image!\n" );
 		success = false;
 	}
 
 	//Load down surface
 	gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ] = loadSurface( "images/cartoon_toast_down.png" );
-	if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ] == NULL )
-	{
+	if ( gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ] == NULL ) {
 		printf( "Failed to load down image!\n" );
 		success = false;
 	}
 
 	//Load left surface
 	gKeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ] = loadSurface( "images/cartoon_toast_left.png" );
-	if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ] == NULL )
-	{
+	if ( gKeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ] == NULL ) {
 		printf( "Failed to load left image!\n" );
 		success = false;
 	}
 
 	//Load right surface
 	gKeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ] = loadSurface( "images/cartoon_toast_right.png" );
-	if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ] == NULL )
-	{
+	if ( gKeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ] == NULL ) {
 		printf( "Failed to load right image!\n" );
 		success = false;
 	}
@@ -124,8 +113,7 @@ bool loadMedia()
 void close()
 {
 	//Deallocate surfaces
-	for( int i = 0; i < KEY_PRESS_SURFACE_TOTAL; ++i )
-	{
+	for ( int i = 0; i < KEY_PRESS_SURFACE_TOTAL; ++i ) {
 		SDL_FreeSurface( gKeyPressSurfaces[ i ] );
 		gKeyPressSurfaces[ i ] = NULL;
 	}
@@ -146,16 +134,12 @@ SDL_Surface* loadSurface( std::string path )
     //Load image at specified path
     SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
 
-    if( loadedSurface == NULL )
-    {
+    if ( loadedSurface == NULL ) {
         printf( "Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
-    }
-    else
-    {
+    } else {
         //Convert surface to screen format
         optimizedSurface = SDL_ConvertSurface( loadedSurface, gScreenSurface->format, 0 );
-        if( optimizedSurface == NULL )
-        {
+        if ( optimizedSurface == NULL ) {
             printf( "Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
         }
 
@@ -170,19 +154,13 @@ SDL_Surface* loadSurface( std::string path )
 int main( int argc, char* args[] )
 {
 	//Start up SDL and create window
-	if( !init() )
-	{
+	if ( !init() ) {
 		printf( "Failed to initialize!\n" );
-	}
-	else
-	{
+	} else {
 		//Load media
-		if( !loadMedia() )
-		{
+		if ( !loadMedia() ) {
 			printf( "Failed to load media!\n" );
-		}
-		else
-		{	
+		} else {	
 			//Main loop flag
 			bool quit = false;
 
@@ -193,41 +171,32 @@ int main( int argc, char* args[] )
 			gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ];
 
 			//While application is running
-			while( !quit )
-			{
+			while ( !quit ) {
 				//Handle events on queue
-				while( SDL_PollEvent( &e ) != 0 )
-				{
+				while ( SDL_PollEvent( &e ) != 0 ) {
 					//User requests quit
-					if( e.type == SDL_QUIT )
-					{
+					if ( e.type == SDL_QUIT ) {
 						quit = true;
 					}
 					//User presses a key
-					else if( e.type == SDL_KEYDOWN )
-					{
+					else if ( e.type == SDL_KEYDOWN ) {
 						//Select surfaces based on key press
-						switch( e.key.keysym.sym )
-						{
+						switch ( e.key.keysym.sym ) {
 							case SDLK_UP:
-							gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ];
-							break;
-
+                                gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ];
+                                break;
 							case SDLK_DOWN:
-							gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ];
-							break;
-
+                                gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ];
+                                break;
 							case SDLK_LEFT:
-							gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ];
-							break;
-
+                                gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ];
+                                break;
 							case SDLK_RIGHT:
-							gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ];
-							break;
-
+                                gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ];
+                                break;
 							default:
-							gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ];
-							break;
+                                gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ];
+                                break;
 						}
 					}
 				}
